f737389112f23a5819c3eb4282a4464696f77c21
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-03 15:56:21 +0200

    [FEAT] js-snack-5

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Ciao a tutti, ecco gli esercizi da svolgere in autonomia oggi pomeriggio. Alessi
 Buon lavoro!
 
 
-1) Dato un array di numeri, creare un nuovo array contenente solo i numeri pari.
+<!-- 1) Dato un array di numeri, creare un nuovo array contenente solo i numeri pari. -->
+
 let arr = [1, 2, 3, 4, 5, 6];
 let evenNumbers = [];
 
@@ -14,7 +15,8 @@ let evenNumbers = [];
 console.log(evenNumbers); // Output previsto: [2, 4, 6]
 
 
-2) Data una stringa ed una lettera, contare quante volte la lettera è ripetuta nella stringa
+<!-- 2) Data una stringa ed una lettera, contare quante volte la lettera è ripetuta nella stringa -->
+
 let str = "What a wonderfull day for pie!";
 let letter = "a";
 let counter = 0;
@@ -24,7 +26,8 @@ let counter = 0;
 console.log(counter); // Output previsto: 3
 
 
-3) Dato un numero n, creare un array con i primi n termini della sequenza di Fibonacci. Le prime due cifre della sequnza sono 0 e 1, e ogni numero successivo è la somma dei due precedenti.
+<!-- 3) Dato un numero n, creare un array con i primi n termini della sequenza di Fibonacci. Le prime due cifre della sequnza sono 0 e 1, e ogni numero successivo è la somma dei due precedenti. -->
+
 let n = 5;
 let fibonacciSeq = [0, 1];
 
@@ -33,7 +36,8 @@ let fibonacciSeq = [0, 1];
 console.log(fibonacciSeq); // Output previsto: [0, 1, 1, 2, 3]
 
 
-4) Data una stringa, creare una nuova stringa trasformando tutte le lettere minuscole in maiuscole e tutte le lettere maiuscole in minuscole.
+<!-- 4) Data una stringa, creare una nuova stringa trasformando tutte le lettere minuscole in maiuscole e tutte le lettere maiuscole in minuscole. -->
+
 let str = "HeLLo WoRLD";
 let transformedStr = "";
 
@@ -42,12 +46,17 @@ let transformedStr = "";
 console.log(transformedStr); // Output previsto: "hEllO wOrld"
 
 
-5) Data una stringa, verificare se questa contiene solo caratteri unici (nessun carattere si ripete). Utilizza una variabile booleana per memorizzare il risultato.
+<!-- 5) Data una stringa, verificare se questa contiene solo caratteri unici (nessun carattere si ripete). Utilizza una variabile booleana per memorizzare il risultato. -->
+
 let str = "abcdefg";
 let hasUniqueChars = true;
 
 // Inserisci qui il tuo codice
 
 console.log(hasUniqueChars); // Output previsto: true
+
+
+
+
 Link al glossario 2.0: https://github.com/fabriziom90/js-glossary.git
 :avviso: NB: Il valore assegnato alle variabili di riferimento per tutti gli esercizio è solo a titolo esemplificativo. Assicurati che il codice restituisca l'output desiderato per qualsiasi input!
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // JS-SNACK-1
-console.log("1) Dato un array di numeri, creare un nuovo array contenente solo i numeri pari");
+console.log("1) Dato un array di numeri, creare un nuovo array contenente solo i numeri pari.");
 
 let arr = [1, 2, 3, 4, 5, 6];
 let evenNumbers = [];
@@ -10,13 +10,13 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 
-console.log(evenNumbers);
+console.log(evenNumbers); // Output previsto: [2, 4, 6]
 
 
 // JS-SNACK-2
-console.log("2) Data una stringa ed una lettera, contare quante volte la lettera è ripetuta nella stringa");
+console.log("2) Data una stringa ed una lettera, contare quante volte la lettera è ripetuta nella stringa.");
 
-// Cambiato Str con StrPie
+// Cambiato "Str" con "StrPie"
 let strPie = "What a wonderful day for pie!";
 let letter = "a";
 let counter = 0;
@@ -27,11 +27,11 @@ for (let i = 0; i < strPie.length; i++) {
     }
 }
 
-console.log(counter);
+console.log(counter); // Output previsto: 3
 
 
 // JS-SNACK-3
-console.log("Dato un numero n, creare un array con i primi n termini della sequenza di Fibonacci. Le prime due cifre della sequenza sono 0 e 1, e ogni numero successivo è la somma dei due precedenti");
+console.log("3) Dato un numero n, creare un array con i primi n termini della sequenza di Fibonacci. Le prime due cifre della sequenza sono 0 e 1, e ogni numero successivo è la somma dei due precedenti.");
 
 let n = 5;
 let fibonacciSeq = [0, 1];
@@ -40,21 +40,39 @@ while (fibonacciSeq.length < n) {
     fibonacciSeq.push(fibonacciSeq[fibonacciSeq.length - 1] + fibonacciSeq[fibonacciSeq.length - 2]);
 }
 
-console.log(fibonacciSeq);
+console.log(fibonacciSeq); // Output previsto: [0, 1, 1, 2, 3]
 
 
 // JS-SNACK-4
-console.log("Data una stringa, creare una nuova stringa trasformando tutte le lettere minuscole in maiuscole e tutte le lettere maiuscole in minuscole");
+console.log("4) Data una stringa, creare una nuova stringa trasformando tutte le lettere minuscole in maiuscole e tutte le lettere maiuscole in minuscole.");
 
-let str = "HeLLo WoRLD";
+// Cambiato "Str" con "StrHello"
+let strHello = "HeLLo WoRLD";
 let transformedStr = "";
 
-for (let i = 0; i < str.length; i++) {
-    if (str[i] === str[i].toLowerCase()) {
-        transformedStr += str[i].toUpperCase();
+for (let i = 0; i < strHello.length; i++) {
+    if (strHello[i] === strHello[i].toLowerCase()) {
+        transformedStr += strHello[i].toUpperCase();
     } else {
-        transformedStr += str[i].toLowerCase();
+        transformedStr += strHello[i].toLowerCase();
     }
 }
 
-console.log(transformedStr);
+console.log(transformedStr); // Output previsto: "hEllO wOrld"
+
+
+// JS-SNACK-5
+console.log("5) Data una stringa, verificare se questa contiene solo caratteri unici (nessun carattere si ripete). Utilizza una variabile booleana per memorizzare il risultato.");
+
+let str = "abcdefg";
+let hasUniqueChars = true;
+
+for (let i = 0; i < str.length - 1; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+        if (str[i] === str[j]) {
+            hasUniqueChars = false;
+        }
+    }
+}
+
+console.log(hasUniqueChars); // Output previsto: true
