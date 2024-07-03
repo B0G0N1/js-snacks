152229b4ae74b0af3ca7aeb177bdac35c0af10ce
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-03 15:41:33 +0200

    [FEAT] js-snack-4

--- js/script.js
+++ js/script.js
@@ -16,12 +16,13 @@ console.log(evenNumbers);
 // JS-SNACK-2
 console.log("2) Data una stringa ed una lettera, contare quante volte la lettera è ripetuta nella stringa");
 
-let str = "What a wonderful day for pie!";
+// Cambiato Str con StrPie
+let strPie = "What a wonderful day for pie!";
 let letter = "a";
 let counter = 0;
 
-for (let i = 0; i < str.length; i++) {
-    if (str[i] === letter) {
+for (let i = 0; i < strPie.length; i++) {
+    if (strPie[i] === letter) {
         counter++;
     }
 }
@@ -32,7 +33,7 @@ console.log(counter);
 // JS-SNACK-3
 console.log("Dato un numero n, creare un array con i primi n termini della sequenza di Fibonacci. Le prime due cifre della sequenza sono 0 e 1, e ogni numero successivo è la somma dei due precedenti");
 
-let n = 5; // Puoi cambiare questo valore per ottenere più o meno termini della sequenza
+let n = 5;
 let fibonacciSeq = [0, 1];
 
 while (fibonacciSeq.length < n) {
@@ -40,3 +41,20 @@ while (fibonacciSeq.length < n) {
 }
 
 console.log(fibonacciSeq);
+
+
+// JS-SNACK-4
+console.log("Data una stringa, creare una nuova stringa trasformando tutte le lettere minuscole in maiuscole e tutte le lettere maiuscole in minuscole");
+
+let str = "HeLLo WoRLD";
+let transformedStr = "";
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toLowerCase()) {
+        transformedStr += str[i].toUpperCase();
+    } else {
+        transformedStr += str[i].toLowerCase();
+    }
+}
+
+console.log(transformedStr);
